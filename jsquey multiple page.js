2f10e84013bef01826447e104b4e8dacba8b7167
dimasanom9708 <34290214+dimasanom9708@users.noreply.github.com>
2017-12-05 18:28:22 -0800

    Update jsquey multiple page.js

--- jsquey multiple page.js
+++ jsquey multiple page.js
@@ -1,9 +1,9 @@
+jQuery(document).ready(function(){
 jQuery('.button_1').click(function(){
 jQuery('.content_1').fadeIn('slow');
   jQuery('.content_2').fadeOut('fast');
 jQuery('.content_3').fadeOut('fast');
 jQuery('.content_4').fadeOut('fast');
-jQuery('body,html').animate({scrollTop : 0});
 jQuery(this).css('background','#F4655F');
 jQuery(this).css('color','#fff');
 jQuery('.button_2').css('background','#fff');
@@ -13,13 +13,11 @@ jQuery('.button_3').css('color','#F4655F');
 jQuery('.button_4').css('background','#fff');
 jQuery('.button_4').css('color','#F4655F');
 });
-
 jQuery('.button_2').click(function(){
 jQuery('.content_1').fadeOut('fast');
   jQuery('.content_2').fadeIn('slow');
 jQuery('.content_3').fadeOut('fast');
 jQuery('.content_4').fadeOut('fast');
-jQuery('body,html').animate({scrollTop : 0});
 jQuery(this).css('background','#F4655F');
 jQuery(this).css('color','#fff');
 jQuery('.button_1').css('background','#fff');
@@ -29,13 +27,11 @@ jQuery('.button_3').css('color','#F4655F');
 jQuery('.button_4').css('background','#fff');
 jQuery('.button_4').css('color','#F4655F');
 });
-
 jQuery('.button_3').click(function(){
 jQuery('.content_1').fadeOut('fast');
   jQuery('.content_2').fadeOut('fast');
   jQuery('.content_4').fadeOut('fast');
 jQuery('.content_3').fadeIn('slow');
-jQuery('body,html').animate({scrollTop : 0});
 jQuery(this).css('background','#F4655F');
 jQuery(this).css('color','#fff');
 jQuery('.button_1').css('background','#fff');
@@ -45,13 +41,11 @@ jQuery('.button_2').css('color','#F4655F');
 jQuery('.button_4').css('background','#fff');
 jQuery('.button_4').css('color','#F4655F');
 });
-
 jQuery('.button_4').click(function(){
 jQuery('.content_1').fadeOut('fast');
   jQuery('.content_2').fadeOut('fast');
   jQuery('.content_3').fadeOut('fast');
 jQuery('.content_4').fadeIn('slow');
-jQuery('body,html').animate({scrollTop : 0});
 jQuery(this).css('background','#F4655F');
 jQuery(this).css('color','#fff');
 jQuery('.button_1').css('background','#fff');
@@ -60,4 +54,5 @@ jQuery('.button_2').css('background','#fff');
 jQuery('.button_2').css('color','#F4655F');
 jQuery('.button_3').css('background','#fff');
 jQuery('.button_3').css('color','#F4655F');
+});
 });
